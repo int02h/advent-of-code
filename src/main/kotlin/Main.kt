16027220ef34aed1eb6d580ea6b812aad7419ca4
Daniil Popov <int02h@gmail.com>
2022-12-03 20:11:55 +0200

    [2022] Day 3: Rucksack Reorganization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,7 @@ fun main() {
 //    aoc2022.Day1.part1(Input(2022, 1))
 //    aoc2022.Day1.part2(Input(2022, 1))
 //    aoc2022.Day2.part1(Input(2022, 2))
-    aoc2022.Day2.part2(Input(2022, 2))
+//    aoc2022.Day2.part2(Input(2022, 2))
+//    aoc2022.Day3.part1(Input(2022, 3))
+    aoc2022.Day3.part2(Input(2022, 3))
 }
