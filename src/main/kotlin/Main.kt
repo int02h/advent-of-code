0d5a1d5d103a65b4919833e9f60405f6b07e807b
Daniil Popov <int02h@gmail.com>
2022-12-05 09:22:42 +0200

    [2022] Day 4: Camp Cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,7 @@ fun main() {
 //    aoc2022.Day2.part1(Input(2022, 2))
 //    aoc2022.Day2.part2(Input(2022, 2))
 //    aoc2022.Day3.part1(Input(2022, 3))
-    aoc2022.Day3.part2(Input(2022, 3))
+//    aoc2022.Day3.part2(Input(2022, 3))
+//    aoc2022.Day4.part1(Input(2022, 4))
+    aoc2022.Day4.part2(Input(2022, 4))
 }
