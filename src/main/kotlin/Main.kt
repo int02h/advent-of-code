927405224583dec1d3b16ebc17ff4f659ce3cd8e
Daniil Popov <int02h@gmail.com>
2022-12-24 13:25:31 +0200

    [2022] Day 23: Unstable Diffusion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -41,6 +41,8 @@ fun main() {
 //    aoc2022.Day20.part2(Input(2022, 20))
 //    aoc2022.Day21.part1(Input(2022, 21))
 //    aoc2022.Day21.part2(Input(2022, 21))
-    aoc2022.Day22.part1(Input(2022, 22))
-    aoc2022.Day22.part2(Input(2022, 22))
+//    aoc2022.Day22.part1(Input(2022, 22))
+//    aoc2022.Day22.part2(Input(2022, 22))
+    aoc2022.Day23.part1(Input(2022, 23))
+    aoc2022.Day23.part2(Input(2022, 23))
 }
