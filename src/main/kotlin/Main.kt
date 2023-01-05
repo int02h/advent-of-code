bc8916535f446b3a8e0b4467f16803af0c0c466d
Daniil Popov <int02h@gmail.com>
2023-01-05 13:25:43 +0200

    [2015] Day 11: Corporate Policy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    aoc2015.Day10.part1(Input(2015, 10))
-    aoc2015.Day10.part2(Input(2015, 10))
+    aoc2015.Day11.part1(Input(2015, 11))
+    aoc2015.Day11.part2(Input(2015, 11))
 }
