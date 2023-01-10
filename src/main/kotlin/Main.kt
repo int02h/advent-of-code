9cdafff6370eda09229c047801be57b7f5ac2aa2
Daniil Popov <int02h@gmail.com>
2023-01-10 15:36:38 +0200

    [2015] Day 15: Science for Hungry People

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    aoc2015.Day14.part1(Input(2015, 14))
-    aoc2015.Day14.part2(Input(2015, 14))
+    aoc2015.Day15.part1(Input(2015, 15))
+    aoc2015.Day15.part2(Input(2015, 15))
 }
