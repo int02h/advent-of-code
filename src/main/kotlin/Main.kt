387f0c17f947f62f93301d44865a1adf6eb2828c
Daniil Popov <int02h@gmail.com>
2023-12-02 16:37:52 +0200

    Improved version of code running

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
+import org.reflections.Reflections
+
 fun main() {
-    aoc2015.Day21.part1(Input(2015, 21))
-    aoc2015.Day21.part2(Input(2015, 21))
+    val year = 2015
+    val reflections = Reflections("aoc${year}")
+    val instance = reflections.getSubTypesOf(AocDay::class.java)
+        .mapNotNull { it.kotlin.objectInstance }
+        .sortedBy { it::class.simpleName }
+        .last()
+    instance.part1(Input(year, getDayIndex(instance)))
+    instance.part2(Input(year, getDayIndex(instance)))
 }
+
+private fun getDayIndex(instance: AocDay): Int = instance::class.simpleName!!.takeLastWhile { it.isDigit() }.toInt()
